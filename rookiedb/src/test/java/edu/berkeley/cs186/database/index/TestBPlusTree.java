4161d61a9f00a9b6a201ff494ed3a31a178c939d
ruiyan-ma <maruiyan17@gmail.com>
2023-03-26 18:02:09 -0700

    add unit test

--- rookiedb/src/test/java/edu/berkeley/cs186/database/index/TestBPlusTree.java
+++ rookiedb/src/test/java/edu/berkeley/cs186/database/index/TestBPlusTree.java
@@ -485,4 +485,26 @@ public class TestBPlusTree {
         assertEquals(3, InnerNode.maxOrder(pageSizeInBytes, keySchema));
         assertEquals(3, BPlusTree.maxOrder(pageSizeInBytes, keySchema));
     }
+
+    @Test
+    public void testDeleteEverything() {
+        // everything should work properly even if we delete everything from the BPlusTree.
+        // For example, after everything is deleted and a new iterator is created, the new
+        // iterator should immediately return false for hasNext.
+
+        BPlusTree tree = getBPlusTree(Type.intType(), 2);
+        for (int i = 0; i < 100; i++) {
+            tree.put(new IntDataBox(i), new RecordId(i, (short) i));
+        }
+
+        Iterator<RecordId> iterBeforeDeleting = tree.scanAll();
+        assertTrue(iterBeforeDeleting.hasNext());
+
+        for (int i = 0; i < 100; i++) {
+            tree.remove(new IntDataBox(i));
+        }
+
+        Iterator<RecordId> iter = tree.scanAll();
+        assertFalse(iter.hasNext());
+    }
 }
